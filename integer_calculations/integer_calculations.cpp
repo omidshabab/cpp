9986ee277cb4e85ab39f4772c01d1a61af32a93b
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2022-12-06 22:45:32 +0330

    first commit

---
+++ integer_calculations/integer_calculations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m=54;
+    int n=20;
+    cout << "m = " << m << " and n = " << n << endl;
+    cout << "m+n = " << m+n << endl;
+    cout << "m-n = " << m-n << endl;
+    cout << "m*n = " << m*n << endl;
+    cout << "m/n = " << m/n << endl;
+    cout << "m%n = " << m%n << endl;
+    return 0;
+}
