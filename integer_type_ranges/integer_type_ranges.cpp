13a830a4b911393e63c9c86c5c2db1b74eec2a4e
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2022-12-06 22:04:58 +0330

    first commit

---
+++ integer_type_ranges/integer_type_ranges.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main(){
+    header:
+        cout << "minimum short = " << SHRT_MIN << endl;
+        cout << "maximum short = " << SHRT_MAX << endl;
+        cout << "minimum unsigned short = 0" << endl;
+        cout << "maximum unsigned short = " << USHRT_MAX << endl;
+        cout << "minimum int = " << INT_MIN << endl;
+        cout << "maximum int = " << INT_MAX << endl;
+        cout << "minimum unsigned int = 0" << endl;
+        cout << "maximum unsigned int = " << UINT_MAX << endl;
+        cout << "minimum long = " << LONG_MIN << endl;
+        cout << "maximum long = " << LONG_MAX << endl;
+        cout << "minimum unsigned long = 0" << endl;
+        cout << "maximum unsigned long = " << ULONG_MAX << endl;
+
+        return 0;
+}
