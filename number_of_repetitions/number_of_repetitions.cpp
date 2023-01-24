77d4150d16d9703359eb95ea0ee46bfe399ab94e
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-01-24 16:28:47 +0330

    first commit

--- number_of_repetitions/number_of_repetitions.cpp
+++ number_of_repetitions/number_of_repetitions.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main(){
-    int x;
-    int a[10];
+    int x, a[10];
 
     for(int i = 0; i<10; i++) a[i]=0;
 
     for( ; ; ){
         cin >> x;
         for(int i=0; i<10; i++) if(i==x) a[i]++;
-        if(x==-1)break;
+        if(x==-1) break;
     }
 
     for(int i=0; i<10; i++) cout << a[i] << " ";
